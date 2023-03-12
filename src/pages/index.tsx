04f65bcf3452aebaefe3f1d1c513a6bf7ea698bd
Leebonggu <leebonggu12@hanmail.net>
2023-03-12 17:15:58 +0900

    remove unuse

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        Remove default markup
         this is staging
       </main>
     </>
