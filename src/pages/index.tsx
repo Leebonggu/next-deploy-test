4371eee6f220c774c12681cc8fc5e88f9ac95d4e
Leebonggu <leebonggu12@hanmail.net>
2023-03-12 17:01:03 +0900

    push to main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        Remove default markup
+        this is main
       </main>
     </>
   )
